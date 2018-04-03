cc411a1c5330dd1ef5c4bf543c2ab43e06914469
Alex Merkulov <merkulov.alex@gmail.com>
2018-04-03 14:07:56 -0400

    Second commit for testing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react',
+    auhor: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+
+  }
+]
+
+
 class App extends Component {
-  render() {
+
+   render() {
+    let name = {
+      fist: "Alex",
+      last: "Merkulov"
+    };
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +35,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started {name.fist}--{name.last}, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
